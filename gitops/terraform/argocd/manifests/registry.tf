21bde9ccbf5f6a199e6d2d7ba2da24bad1970ef9
johndietz <john@kubefirst.com>
2021-09-01 20:54:38 -0400

    set recurse_directory=true on registry

--- gitops/terraform/argocd/manifests/registry.tf
+++ gitops/terraform/argocd/manifests/registry.tf
@@ -1,6 +1,7 @@
 module "registry" {
   source = "../templates/argocd-app"
 
-  app_name      = "registry"
-  resource_path = "registry"
+  app_name          = "registry"
+  resource_path     = "registry"
+  recurse_directory = true
 }
