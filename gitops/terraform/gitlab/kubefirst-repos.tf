34f535090cf0b3ede61661f4db2cc0d3cf180b9b
johndietz <john@kubefirst.com>
2021-09-01 20:56:54 -0400

    initialize_with_readme=false on gitops

--- gitops/terraform/gitlab/kubefirst-repos.tf
+++ gitops/terraform/gitlab/kubefirst-repos.tf
@@ -37,7 +37,7 @@ module "gitops" {
   group_name                            = gitlab_group.kubefirst.id
   repo_name                             = "gitops"
   create_ecr                            = true
-  initialize_with_readme                = true
+  initialize_with_readme                = false
   only_allow_merge_if_pipeline_succeeds = false
   remove_source_branch_after_merge      = true
 }
