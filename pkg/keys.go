e24141365fce1f89c92863f4d91c7bdd1ae85bec
John Dietz <john@kubefirst.com>
2022-07-14 13:06:20 -0400

    adding retry spec to argocd registry (#102)

--- pkg/keys.go
+++ pkg/keys.go
@@ -1,37 +1,37 @@
 package pkg
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
-	goGitSsh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
-	"github.com/kubefirst/kubefirst/configs"
-	"github.com/spf13/viper"
-	"golang.org/x/crypto/ssh"
-	"io/ioutil"
-	"log"
-	"strings"
+  "crypto/rand"
+  "crypto/rsa"
+  "crypto/x509"
+  "encoding/pem"
+  "fmt"
+  goGitSsh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
+  "github.com/kubefirst/kubefirst/configs"
+  "github.com/spf13/viper"
+  "golang.org/x/crypto/ssh"
+  "io/ioutil"
+  "log"
+  "strings"
 )
 
 func CreateSshKeyPair() {
-	config := configs.ReadConfig()
-	publicKey := viper.GetString("botpublickey")
-	if publicKey == "" {
-		log.Println("generating new key pair")
-		publicKey, privateKey, _ := GenerateKey()
-		viper.Set("botPublicKey", publicKey)
-		viper.Set("botPrivateKey", privateKey)
-		err := viper.WriteConfig()
-		if err != nil {
-			log.Panicf("error: could not write to viper config")
-		}
-	}
-	publicKey = viper.GetString("botpublickey")
-	privateKey := viper.GetString("botprivatekey")
+  config := configs.ReadConfig()
+  publicKey := viper.GetString("botpublickey")
+  if publicKey == "" {
+    log.Println("generating new key pair")
+    publicKey, privateKey, _ := GenerateKey()
+    viper.Set("botPublicKey", publicKey)
+    viper.Set("botPrivateKey", privateKey)
+    err := viper.WriteConfig()
+    if err != nil {
+      log.Panicf("error: could not write to viper config")
+    }
+  }
+  publicKey = viper.GetString("botpublickey")
+  privateKey := viper.GetString("botprivatekey")
 
-	var argocdInitValuesYaml = []byte(fmt.Sprintf(`
+  var argocdInitValuesYaml = []byte(fmt.Sprintf(`
 server:
   additionalApplications:
   - name: registry
@@ -54,6 +54,12 @@ server:
         selfHeal: true
       syncOptions:
       - CreateNamespace=true
+      retry:
+        limit: 5
+        backoff:
+          duration: 5s
+          maxDuration: 5m0s
+          factor: 2
 configs:
   repositories:
     soft-serve-gitops:
@@ -68,57 +74,57 @@ configs:
         %s
 `, strings.ReplaceAll(privateKey, "\n", "\n        ")))
 
-	err := ioutil.WriteFile(fmt.Sprintf("%s/argocd-init-values.yaml", config.K1srtFolderPath), argocdInitValuesYaml, 0644)
-	if err != nil {
-		log.Panicf("error: could not write argocd-init-values.yaml %s", err)
-	}
+  err := ioutil.WriteFile(fmt.Sprintf("%s/argocd-init-values.yaml", config.K1srtFolderPath), argocdInitValuesYaml, 0644)
+  if err != nil {
+    log.Panicf("error: could not write argocd-init-values.yaml %s", err)
+  }
 }
 
 func PublicKey() (*goGitSsh.PublicKeys, error) {
-	var publicKey *goGitSsh.PublicKeys
-	publicKey, err := goGitSsh.NewPublicKeys("gitClient", []byte(viper.GetString("botprivatekey")), "")
-	if err != nil {
-		return nil, err
-	}
-	return publicKey, err
+  var publicKey *goGitSsh.PublicKeys
+  publicKey, err := goGitSsh.NewPublicKeys("gitClient", []byte(viper.GetString("botprivatekey")), "")
+  if err != nil {
+    return nil, err
+  }
+  return publicKey, err
 }
 
 // GenerateKey generate public and private keys to be consumed by GitLab.
 func GenerateKey() (string, string, error) {
-	reader := rand.Reader
-	bitSize := 2048
+  reader := rand.Reader
+  bitSize := 2048
 
-	key, err := rsa.GenerateKey(reader, bitSize)
-	if err != nil {
-		return "", "", err
-	}
+  key, err := rsa.GenerateKey(reader, bitSize)
+  if err != nil {
+    return "", "", err
+  }
 
-	pub, err := ssh.NewPublicKey(key.Public())
-	if err != nil {
-		return "", "", err
-	}
-	publicKey := string(ssh.MarshalAuthorizedKey(pub))
-	// encode RSA key
-	privateKey := string(pem.EncodeToMemory(
-		&pem.Block{
-			Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key),
-		},
-	))
+  pub, err := ssh.NewPublicKey(key.Public())
+  if err != nil {
+    return "", "", err
+  }
+  publicKey := string(ssh.MarshalAuthorizedKey(pub))
+  // encode RSA key
+  privateKey := string(pem.EncodeToMemory(
+    &pem.Block{
+      Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key),
+    },
+  ))
 
-	return publicKey, privateKey, nil
+  return publicKey, privateKey, nil
 }
 
 func ModConfigYaml() {
 
-	file, err := ioutil.ReadFile("./config.yaml")
-	if err != nil {
-		log.Println("error reading file", err)
-	}
+  file, err := ioutil.ReadFile("./config.yaml")
+  if err != nil {
+    log.Println("error reading file", err)
+  }
 
-	newFile := strings.Replace(string(file), "allow-keyless: false", "allow-keyless: true", -1)
+  newFile := strings.Replace(string(file), "allow-keyless: false", "allow-keyless: true", -1)
 
-	err = ioutil.WriteFile("./config.yaml", []byte(newFile), 0)
-	if err != nil {
-		panic(err)
-	}
+  err = ioutil.WriteFile("./config.yaml", []byte(newFile), 0)
+  if err != nil {
+    panic(err)
+  }
 }
