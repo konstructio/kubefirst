3dd16785628427fb91bf0b20cb385b5a53b9d04f
Thiago Pagotto <pagottoo@gmail.com>
2022-07-19 15:23:56 -0300

    fix cmd description
    Signed-off-by: Thiago Pagotto <pagottoo@gmail.com>

--- cmd/backupSsl.go
+++ cmd/backupSsl.go
@@ -12,7 +12,7 @@ import (
 var backupSslCmd = &cobra.Command{
 	Use:   "backupSSL",
 	Short: "Backup Secrets (cert-manager/certificates) to bucket k1-<DOMAIN>",
-	Long: `This command create a backupt of secrets from certmanager certificates to bucket named kubefirst-<DOMAIN> 
+	Long: `This command create a backupt of secrets from certmanager certificates to bucket named k1-<DOMAIN> 
 where can be used on provisioning phase with the flag --recycle-ssl`,
 
 	Run: func(cmd *cobra.Command, args []string) {
