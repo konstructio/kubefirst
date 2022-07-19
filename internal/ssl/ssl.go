58c0a19a536cc5b2a067e65633fb30b4fca90b12
Thiago Pagotto <pagottoo@gmail.com>
2022-07-19 09:13:26 -0300

    find all resourcer to backup cert-manager objects

--- internal/ssl/ssl.go
+++ internal/ssl/ssl.go
@@ -3,16 +3,21 @@ package ssl
 import (
 	"context"
 	"fmt"
+	"log"
+	"path/filepath"
 
 	"github.com/ghodss/yaml"
 	"github.com/kubefirst/kubefirst/configs"
+	"github.com/kubefirst/kubefirst/internal/aws"
 	"github.com/kubefirst/kubefirst/internal/k8s"
+	"github.com/kubefirst/kubefirst/pkg"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// GetBackupCertificates create a backup of Certificates on AWS S3 in yaml files
-func GetBackupCertificates(namespaces []string) ([]string, error) {
+func getItemsToBackup(apiGroup string, apiVersion string, resourceType string, namespaces []string, jqQuery string) ([]string, error) {
 	config := configs.ReadConfig()
 
 	k8sConfig, err := clientcmd.BuildConfigFromFlags("", config.KubeConfigPath)
@@ -21,22 +26,18 @@ func GetBackupCertificates(namespaces []string) ([]string, error) {
 	}
 
 	k8sClient := dynamic.NewForConfigOrDie(k8sConfig)
-	var files []string
-	for _, namespace := range namespaces {
-		// items, err := k8s.GetResourcesDynamically(k8sClient, context.TODO(),
-		// 	"cert-manager.io", "v1", "certificates", namespace)
-		// if err != nil {
-		// 	return nil, fmt.Errorf("error getting resources from k8s: %s", err)
-		// }
 
-		k8sResourceTypes := []string{
-			"certificates",
-			//"issuer",
-			//"clusterissuer",
+	var files []string
+	var items []unstructured.Unstructured
+	for _, namespace := range namespaces {
+		if len(jqQuery) > 0 {
+			fmt.Println("getting resources and filtering using jq")
+			items, err = k8s.GetResourcesByJq(k8sClient, context.TODO(), apiGroup, apiVersion, resourceType, namespace, jqQuery)
+		} else {
+			fmt.Println("getting resources")
+			items, err = k8s.GetResourcesDynamically(k8sClient, context.TODO(), apiGroup, apiVersion, resourceType, namespace)
 		}
 
-		items, err := k8s.GetResourcesDynamically(k8sClient, context.TODO(),
-			"cert-manager.io", "v1", k8sResourceTypes, namespace)
 		if err != nil {
 			return nil, fmt.Errorf("error getting resources from k8s: %s", err)
 		}
@@ -51,19 +52,65 @@ func GetBackupCertificates(namespaces []string) ([]string, error) {
 			if err != nil {
 				return nil, fmt.Errorf("error converting object from json to yaml: %s", err)
 			}
-			fmt.Println("---debug---")
-			fmt.Println(string(yamlObj))
-			fmt.Println("---debug---")
 
-			// todo: uncomment after unblocking the code above
-			//fileName := fmt.Sprintf("%s.%s", item.GetName(), "yaml")
-			//err = pkg.CreateFile(fileName, yamlObj)
-			//if err != nil {
-			//	return nil, err
-			//}
-			//files = append(files, fileName)
+			fileName := fmt.Sprintf("%s.%s", item.GetName(), "yaml")
+			//TODO: test if kubeconfigpath is the correct value to write the files together another k1rst files
+			fullFileName := filepath.Join(config.KubeConfigPath, fileName)
+			err = pkg.CreateFile(fullFileName, yamlObj)
+			if err != nil {
+				return nil, err
+			}
+			files = append(files, fullFileName)
 		}
 	}
 
 	return files, nil
+}
+
+//func GetBackupCertificates(apiGroup string, apiVersion string, resourceTypes []string, namespace string) ([]string, error) {
+// GetBackupCertificates create a backup of Certificates on AWS S3 in yaml files
+func GetBackupCertificates() (string, error) {
+
+	fmt.Println("GetBackupCertificates called")
+	bucketName := fmt.Sprintf("k1-%s", viper.GetString("aws.hostedzonename"))
+	path := "cert-manager"
+	aws.CreateBucket(false, bucketName)
+
+	fmt.Println("getting certificates")
+	namespaces := []string{"argo", "atlantis", "chartmuseum", "gitlab", "vault"}
+	certificates, err := getItemsToBackup("cert-manager.io", "v1", "certificates", namespaces, "")
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, cert := range certificates {
+		fullPath := fmt.Sprintf("%s/cert-%s", path, cert)
+		fmt.Println(fullPath)
+		aws.UploadFile(bucketName, fullPath, cert)
+	}
+
+	fmt.Println("getting secrets")
+	query := ".metadata.annotations[\"cert-manager.io/issuer-kind\"] == \"ClusterIssuer\""
+	secrets, err := getItemsToBackup("", "v1", "secrets", namespaces, query)
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, secret := range secrets {
+		fullPath := fmt.Sprintf("%s/secret-%s", path, secret)
+		fmt.Println(fullPath)
+		aws.UploadFile(bucketName, fullPath, secret)
+	}
+
+	emptyNS := []string{""}
+	fmt.Println("getting clusterissuers")
+	clusterIssuers, err := getItemsToBackup("cert-manager.io", "v1", "clusterissuers", emptyNS, "")
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, clusterissuer := range clusterIssuers {
+		fullPath := fmt.Sprintf("%s/clusterissuer-%s", path, clusterissuer)
+		fmt.Println(fullPath)
+		aws.UploadFile(bucketName, fullPath, clusterissuer)
+	}
+
+	return "Backuped Cert-Manager resources finished successfully!", nil
 }
