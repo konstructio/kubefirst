c7f8ab954aff98704b72831dc0bb314dcf3e95b4
Thiago Pagotto <pagottoo@gmail.com>
2022-07-19 09:12:24 -0300

    add func to query resources with jq

--- internal/k8s/kubernetes.go
+++ internal/k8s/kubernetes.go
@@ -1,3 +1,7 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package k8s
 
 import (
@@ -9,10 +13,12 @@ import (
 	"os/exec"
 	"time"
 
+	"github.com/itchyny/gojq"
 	"github.com/kubefirst/kubefirst/internal/argocd"
 	"github.com/spf13/viper"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	coreV1Types "k8s.io/client-go/kubernetes/typed/core/v1"
@@ -92,26 +98,70 @@ func GetResourcesDynamically(dynamic dynamic.Interface,
 	ctx context.Context,
 	group string,
 	version string,
-	resources []string,
-	namespace string,
-) ([]unstructured.UnstructuredList, error) {
+	resource string,
+	namespace string) (
+	[]unstructured.Unstructured, error) {
 
-	var listTypes []unstructured.UnstructuredList
+	resourceId := schema.GroupVersionResource{
+		Group:    group,
+		Version:  version,
+		Resource: resource,
+	}
+	list, err := dynamic.Resource(resourceId).Namespace(namespace).
+		List(ctx, metaV1.ListOptions{})
 
-	for _, resource := range resources {
+	if err != nil {
+		return nil, err
+	}
 
-		resourceId := schema.GroupVersionResource{
-			Group:    group,
-			Version:  version,
-			Resource: resource,
-		}
+	return list.Items, nil
+}
 
-		resourceList, err := dynamic.Resource(resourceId).Namespace(namespace).List(ctx, metaV1.ListOptions{})
+func GetResourcesByJq(dynamic dynamic.Interface, ctx context.Context, group string,
+	version string, resource string, namespace string, jq string) (
+	[]unstructured.Unstructured, error) {
+
+	resources := make([]unstructured.Unstructured, 0)
+
+	query, err := gojq.Parse(jq)
+	if err != nil {
+		return nil, err
+	}
+
+	items, err := GetResourcesDynamically(dynamic, ctx, group, version, resource, namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range items {
+
+		// Convert object to raw JSON
+		var rawJson interface{}
+		err = runtime.DefaultUnstructuredConverter.FromUnstructured(item.Object, &rawJson)
 		if err != nil {
 			return nil, err
 		}
-		listTypes = append(listTypes, *resourceList)
-	}
 
-	return listTypes, nil
+		// Evaluate jq against JSON
+		iter := query.Run(rawJson)
+		for {
+			result, ok := iter.Next()
+			if !ok {
+				break
+			}
+			if err, ok := result.(error); ok {
+				if err != nil {
+					return nil, err
+				}
+			} else {
+				boolResult, ok := result.(bool)
+				if !ok {
+					fmt.Println("Query returned non-boolean value")
+				} else if boolResult {
+					resources = append(resources, item)
+				}
+			}
+		}
+	}
+	return resources, nil
 }
