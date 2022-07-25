725d26e5b84d7b237ad62238afb4c7e9b1a60a56
Thiago Pagotto <pagottoo@gmail.com>
2022-07-25 08:50:10 -0300

    add func to ask confirmations
    Signed-off-by: Thiago Pagotto <pagottoo@gmail.com>

--- pkg/helpers.go
+++ pkg/helpers.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -152,4 +153,25 @@ func CreateFullPath(p string) (*os.File, error) {
 		return nil, err
 	}
 	return os.Create(p)
+}
+
+func AskForConfirmation(s string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/n]: ", s)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		}
+	}
 }
