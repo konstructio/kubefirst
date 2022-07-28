c694c127db44301bb61a98ca1840ebe7876713ee
Cesar Filho <53096417+6za@users.noreply.github.com>
2022-07-28 07:19:37 -0300

    Testing a wrapper for github (#166)
    * Testing a wrapper for github
    
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>
    
    * Testing a wrapper for github
    
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>
    
    * Testing a wrapper for github
    
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>
    
    * Testing a wrapper for github
    
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- cmd/createGithub.go
+++ cmd/createGithub.go
@@ -5,18 +5,14 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/kubefirst/kubefirst/configs"
+	"github.com/kubefirst/kubefirst/internal/githubWrapper"
 	"github.com/kubefirst/kubefirst/internal/progressPrinter"
 	"github.com/spf13/cobra"
-	"golang.org/x/oauth2"
-
-	"github.com/google/go-github/v45/github"
 )
 
 // createGithubCmd represents the createGithub command
@@ -29,6 +25,7 @@ var createGithubCmd = &cobra.Command{
 		progressPrinter.GetInstance()
 		progressPrinter.SetupProgress(4)
 		config := configs.ReadConfig()
+		gitWrapper := githubWrapper.New()
 		log.Printf(config.AwsProfile)
 		infoCmd.Run(cmd, args)
 
@@ -36,7 +33,7 @@ var createGithubCmd = &cobra.Command{
 		//sendStartedInstallTelemetry(dryRun, useTelemetry)
 		informUser("Create Github Org")
 		informUser("Create Github Repo - gitops")
-		createRepo("gitops")
+		gitWrapper.CreatePrivateRepo("", "sample", "My First App")
 		//gitlab.PushGitRepo(dryRun, config, "gitlab", "metaphor")
 		// make a github version of it
 		informUser("Create Github Repo - metaphor")
@@ -73,31 +70,4 @@ var createGithubCmd = &cobra.Command{
 func init() {
 	clusterCmd.AddCommand(createGithubCmd)
 
-}
-
-func createRepo(name string) {
-	token := os.Getenv("GITHUB_AUTH_TOKEN")
-	if token == "" {
-		log.Fatal("Unauthorized: No token present")
-	}
-	if name == "" {
-		log.Fatal("No name: New repos must be given a name")
-	}
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-	isPrivate := true
-	autoInit := true
-	description := "sample"
-	organization := os.Getenv("ORG")
-	r := &github.Repository{Name: &name,
-		Private:     &isPrivate,
-		Description: &description,
-		AutoInit:    &autoInit}
-	repo, _, err := client.Repositories.Create(ctx, organization, r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Successfully created new repo: %v\n", repo.GetName())
 }
