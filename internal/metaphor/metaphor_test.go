25274bed8fd6e18e9e21d9252a294a10b8807fd4
João Vanzuita <joao@kubeshop.io>
2022-08-25 16:41:45 -0300

    chore: add argocd, argo workflow, gitlab, and metaphors integration test
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

---
+++ internal/metaphor/metaphor_test.go
@@ -0,0 +1,68 @@
+package metaphor_test
+
+import (
+	"fmt"
+	"github.com/kubefirst/kubefirst/configs"
+	"github.com/kubefirst/kubefirst/pkg"
+	"github.com/spf13/viper"
+	"net/http"
+	"testing"
+)
+
+func TestMetaphorsLivenessIntegration(t *testing.T) {
+
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	config := configs.ReadConfig()
+	err := pkg.SetupViper(config)
+	if err != nil {
+		t.Error(err)
+	}
+
+	type conditions struct {
+		serviceName    string
+		serviceURL     string
+		httpWantedCode int
+	}
+
+	testCases := []conditions{
+		{
+			serviceName:    "metaphor-frontend",
+			serviceURL:     fmt.Sprintf("https://metaphor-frontend.%s", viper.GetString("aws.hostedzonename")),
+			httpWantedCode: http.StatusOK,
+		},
+		{
+			serviceName:    "metaphor-js",
+			serviceURL:     fmt.Sprintf("https://metaphor-js.%s", viper.GetString("aws.hostedzonename")),
+			httpWantedCode: http.StatusOK,
+		},
+		{
+			serviceName:    "metaphor-go",
+			serviceURL:     fmt.Sprintf("https://metaphor-go.%s", viper.GetString("aws.hostedzonename")),
+			httpWantedCode: http.StatusOK,
+		},
+	}
+
+	for _, wanted := range testCases {
+		t.Run(wanted.serviceName, func(t *testing.T) {
+			t.Parallel()
+
+			req, err := http.NewRequest(http.MethodGet, wanted.serviceURL, nil)
+			if err != nil {
+				t.Error(err)
+			}
+
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if res.StatusCode != wanted.httpWantedCode {
+				t.Errorf("wanted http status code 200, got %d", res.StatusCode)
+			}
+		})
+	}
+
+}
