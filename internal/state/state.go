50ff60dc22e9a227b3993ab41cb33c04f9b96658
João Vanzuita <joao@kubeshop.io>
2022-08-29 10:53:22 -0300

    chore: fix typo
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"fmt"
 	"github.com/kubefirst/kubefirst/configs"
 	"github.com/kubefirst/kubefirst/internal/aws"
 	"github.com/spf13/viper"
@@ -18,8 +19,7 @@ func UploadKubefirstToStateStore(dryRun bool) error {
 	stateStoreBucket := viper.GetString("bucket.state-store.name")
 	err := aws.UploadFile(stateStoreBucket, config.KubefirstConfigFileName, config.KubefirstConfigFilePath)
 	if err != nil {
-		log.Printf("unable to upload Kubefirst cofiguration file to the S3 bucket, error is: %v", err)
-		return err
+		return fmt.Errorf("unable to upload Kubefirst cofiguration file to the S3 bucket, error is: %v", err)
 	}
 	log.Printf("Kubefirst configuration file was upload to AWS S3 at %q bucket name", stateStoreBucket)
 
