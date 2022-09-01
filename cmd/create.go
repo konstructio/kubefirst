13210e716ebd83190fd94b7d1c7ac8f64e5bcbb5
João Vanzuita <joao@kubeshop.io>
2022-09-01 16:41:12 -0300

    chore: update user output
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- cmd/create.go
+++ cmd/create.go
@@ -62,8 +62,8 @@ var createCmd = &cobra.Command{
 		sendCompleteInstallTelemetry(globalFlags.DryRun, globalFlags.UseTelemetry)
 		reports.HandoffScreen(globalFlags.DryRun, globalFlags.SilentMode)
 		time.Sleep(time.Millisecond * 2000)
-		log.Println("End of creation run")
-		informUser("End of creation run", globalFlags.SilentMode)
+		log.Println("Kubefirst installation finished successfully")
+		informUser("Kubefirst installation finished successfully", globalFlags.SilentMode)
 		return nil
 	},
 }
