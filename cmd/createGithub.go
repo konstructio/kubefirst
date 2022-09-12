c9d4c1ea5a154feb06a7824f8c0e1f122ac0be83
6za <53096417+6za@users.noreply.github.com>
2022-09-12 17:07:30 +0000

    fix templates updates
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- cmd/createGithub.go
+++ cmd/createGithub.go
@@ -52,7 +52,7 @@ var createGithubCmd = &cobra.Command{
 		progressPrinter.AddTracker("step-0", "Process Parameters", 1)
 		progressPrinter.AddTracker("step-github", "Setup gitops on github", 3)
 		progressPrinter.AddTracker("step-base", "Setup base cluster", 2)
-		progressPrinter.AddTracker("step-ecr", "Setup ECR/Docker Registries", 4)
+		progressPrinter.AddTracker("step-ecr", "Setup ECR/Docker Registries", 1)
 		progressPrinter.AddTracker("step-apps", "Install apps to cluster", 6)
 
 		progressPrinter.IncrementTracker("step-0", 1)
