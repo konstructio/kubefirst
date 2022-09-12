4de1b3ecdccc554a02fb41e3c3a9358ce9ad2850
Thiago Pagotto <pagottoo@gmail.com>
2022-09-12 08:41:11 -0300

    funcs to create/destroy ecr via terraform
    Signed-off-by: Thiago Pagotto <pagottoo@gmail.com>

--- internal/terraform/terraform.go
+++ internal/terraform/terraform.go
@@ -104,3 +104,71 @@ func DestroyBaseTerraform(skipBaseTerraform bool) {
 		log.Println("skip:  destroyBaseTerraform")
 	}
 }
+
+func ApplyECRTerraform(dryRun bool, directory string) {
+
+	config := configs.ReadConfig()
+
+	if !viper.GetBool("create.terraformapplied.ecr") {
+		log.Println("Executing applyECRTerraform")
+		if dryRun {
+			log.Printf("[#99] Dry-run mode, applyECRTerraform skipped.")
+			return
+		}
+
+		//* AWS_SDK_LOAD_CONFIG=1
+		//* https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs#shared-credentials-file
+		envs := map[string]string{}
+		envs["AWS_SDK_LOAD_CONFIG"] = "1"
+		envs["AWS_PROFILE"] = viper.GetString("aws.profile")
+		envs["TF_VAR_aws_region"] = viper.GetString("aws.region")
+
+		directory = fmt.Sprintf("%s/gitops/terraform/ecr", config.K1FolderPath)
+		err := os.Chdir(directory)
+		if err != nil {
+			log.Panic("error: could not change directory to " + directory)
+		}
+		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "init")
+		if err != nil {
+			log.Panicf("error: terraform init for ecr failed %s", err)
+		}
+
+		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "apply", "-auto-approve")
+		if err != nil {
+			log.Panicf("error: terraform apply for ecr failed %s", err)
+		}
+		os.RemoveAll(fmt.Sprintf("%s/.terraform", directory))
+		viper.Set("create.terraformapplied.ecr", true)
+		viper.WriteConfig()
+	} else {
+		log.Println("Skipping: applyECRTerraform")
+	}
+}
+
+func DestroyECRTerraform(skipECRTerraform bool) {
+	config := configs.ReadConfig()
+	if !skipECRTerraform {
+		directory := fmt.Sprintf("%s/gitops/terraform/ecr", config.K1FolderPath)
+		err := os.Chdir(directory)
+		if err != nil {
+			log.Panicf("error: could not change directory to " + directory)
+		}
+
+		envs := map[string]string{}
+		envs["AWS_PROFILE"] = viper.GetString("aws.profile")
+
+		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "init")
+		if err != nil {
+			log.Println("[WARN]: failed to terraform init (destroy) ECR, was the ECR not created(check AWS)?: %v", err)
+		}
+
+		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "destroy", "-auto-approve")
+		if err != nil {
+			log.Println("[WARN]: failed to terraform destroy ECR, was the ECR not created (check AWS)?: %v", err)
+		}
+		viper.Set("destroy.terraformdestroy.ecr", true)
+		viper.WriteConfig()
+	} else {
+		log.Println("skip:  destroyBaseTerraform")
+	}
+}
