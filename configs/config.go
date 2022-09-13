fca73c26999f587d2f2c018141857974b961d71e
CristhianF7 <CristhianF7@gmail.com>
2022-09-13 13:59:07 -0500

    feat: using latest console release
    Signed-off-by: CristhianF7 <CristhianF7@gmail.com>

--- configs/config.go
+++ configs/config.go
@@ -80,7 +80,7 @@ func ReadConfig() *Config {
 	config.HelmClientPath = fmt.Sprintf("%s/tools/helm", config.K1FolderPath)
 	config.CertsPath = fmt.Sprintf("%s/ssl", config.K1FolderPath)
 	config.TerraformVersion = "1.0.11"
-	config.ConsoleVersion = "0.1.3"
+	config.ConsoleVersion = "0.1.5"
 	config.ArgoCDChartHelmVersion = "4.10.5"
 	// todo adopt latest helmVersion := "v3.9.0"
 	config.HelmVersion = "v3.6.1"
