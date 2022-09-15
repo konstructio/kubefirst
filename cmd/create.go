518594998d0f570b451ae9210f013e82c41f2d4c
Cesar Filho <53096417+6za@users.noreply.github.com>
2022-09-15 16:21:38 -0300

    #386 fix (#403)
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>
    
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- cmd/create.go
+++ cmd/create.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/kubefirst/kubefirst/internal/gitlab"
 	"github.com/kubefirst/kubefirst/internal/state"
 
 	"github.com/kubefirst/kubefirst/internal/flagset"
@@ -51,6 +52,16 @@ var createCmd = &cobra.Command{
 				return err
 			}
 
+		}
+		// Relates to issue: https://github.com/kubefirst/kubefirst/issues/386
+		// Metaphor needs chart museum for CI works
+		informUser("Waiting chartmuseum", globalFlags.SilentMode)
+		for i := 1; i < 10; i++ {
+			chartMuseum := gitlab.AwaitHostNTimes("chartmuseum", globalFlags.DryRun, 20)
+			if chartMuseum {
+				informUser("Chartmuseum DNS is ready", globalFlags.SilentMode)
+				break
+			}
 		}
 		informUser("Deploying metaphor applications", globalFlags.SilentMode)
 		err = deployMetaphorCmd.RunE(cmd, args)
