d71fe5109174984bb569bb37d26ad010468c1cbd
6za <53096417+6za@users.noreply.github.com>
2022-09-22 12:53:21 +0000

    add flagset
    Signed-off-by: 6za <53096417+6za@users.noreply.github.com>

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -27,17 +27,7 @@ Optional: skip gitlab terraform
 if the registry has already been deleted.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		skipGitlabTerraform, err := cmd.Flags().GetBool("skip-gitlab-terraform")
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		skipDeleteRegistryApplication, err := cmd.Flags().GetBool("skip-delete-register")
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		skipBaseTerraform, err := cmd.Flags().GetBool("skip-base-terraform")
+		destroyFlags, err := flagset.ProcessDestroyFlags(cmd)
 		if err != nil {
 			log.Println(err)
 			return err
@@ -60,9 +50,9 @@ if the registry has already been deleted.`,
 		progressPrinter.SetupProgress(2, globalFlags.SilentMode)
 
 		if globalFlags.DryRun {
-			skipGitlabTerraform = true
-			skipDeleteRegistryApplication = true
-			skipBaseTerraform = true
+			destroyFlags.SkipGitlabTerraform = true
+			destroyFlags.SkipDeleteRegistryApplication = true
+			destroyFlags.SkipBaseTerraform = true
 		}
 		progressPrinter.AddTracker("step-prepare", "Open Ports", 3)
 
@@ -74,7 +64,7 @@ if the registry has already been deleted.`,
 		progressPrinter.AddTracker("step-destroy", "Destroy Cloud", 4)
 		progressPrinter.IncrementTracker("step-destroy", 1)
 		informUser("Destroying Gitlab", globalFlags.SilentMode)
-		if !skipGitlabTerraform {
+		if !destroyFlags.SkipGitlabTerraform {
 			kPortForward, _ := k8s.PortForward(globalFlags.DryRun, "gitlab", "svc/gitlab-webservice-default", "8888:8080")
 			defer func() {
 				if kPortForward != nil {
@@ -85,14 +75,14 @@ if the registry has already been deleted.`,
 			informUser("Open gitlab port-forward", globalFlags.SilentMode)
 			progressPrinter.IncrementTracker("step-prepare", 1)
 
-			gitlab.DestroyGitlabTerraform(skipGitlabTerraform)
+			gitlab.DestroyGitlabTerraform(destroyFlags.SkipGitlabTerraform)
 		}
 		progressPrinter.IncrementTracker("step-destroy", 1)
 
 		log.Println("gitlab terraform destruction complete")
 
 		//This should wrapped into a function, maybe to move to: k8s.DeleteRegistryApplication
-		if !skipDeleteRegistryApplication {
+		if !destroyFlags.SkipDeleteRegistryApplication {
 			kPortForwardArgocd, _ := k8s.PortForward(globalFlags.DryRun, "argocd", "svc/argocd-server", "8080:80")
 			defer func() {
 				if kPortForwardArgocd != nil {
@@ -129,7 +119,7 @@ if the registry has already been deleted.`,
 			log.Println("deleting registry application in argocd")
 			// delete argocd registry
 			informUser("Destroying Registry Application", globalFlags.SilentMode)
-			k8s.DeleteRegistryApplication(skipDeleteRegistryApplication)
+			k8s.DeleteRegistryApplication(destroyFlags.SkipDeleteRegistryApplication)
 			progressPrinter.IncrementTracker("step-destroy", 1)
 			log.Println("registry application deleted")
 		}
@@ -140,7 +130,7 @@ if the registry has already been deleted.`,
 
 		log.Println("terraform destroy base")
 		informUser("Destroying Cluster", globalFlags.SilentMode)
-		terraform.DestroyBaseTerraform(skipBaseTerraform)
+		terraform.DestroyBaseTerraform(destroyFlags.SkipBaseTerraform)
 		progressPrinter.IncrementTracker("step-destroy", 1)
 		informUser("All Destroyed", globalFlags.SilentMode)
 
