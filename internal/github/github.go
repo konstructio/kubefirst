4646376482580a5e5ae3083aa95d743042ecf675
Jared Edwards <jared@kubefirst.com>
2022-09-28 14:29:02 -0600

    changes to github terraform invocation

--- internal/github/github.go
+++ internal/github/github.go
@@ -10,44 +10,43 @@ import (
 	"github.com/spf13/viper"
 )
 
-func ApplyGitHubTerraform(dryRun bool, atlantisWebhookSecret string) {
+func ApplyGitHubTerraform(dryRun bool) {
 
 	config := configs.ReadConfig()
 
-	if !viper.GetBool("create.terraformapplied.github") {
-		log.Println("Executing ApplyGithubTerraform")
-		if dryRun {
-			log.Printf("[#99] Dry-run mode, ApplyGithubTerraform skipped.")
-			return
-		}
-		//* AWS_SDK_LOAD_CONFIG=1
-		//* https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs#shared-credentials-file
-		envs := map[string]string{}
-		envs["AWS_SDK_LOAD_CONFIG"] = "1"
-		envs["AWS_PROFILE"] = viper.GetString("aws.profile")
-		// Prepare for terraform gitlab execution
-		envs["GITHUB_TOKEN"] = viper.GetString("github.token")
-		envs["GITHUB_OWNER"] = viper.GetString("github.owner")
-		envs["TF_VAR_atlantis_repo_webhook_secret"] = atlantisWebhookSecret
-
-		directory := fmt.Sprintf("%s/gitops/terraform/github", config.K1FolderPath)
-		err := os.Chdir(directory)
-		if err != nil {
-			log.Panic("error: could not change directory to " + directory)
-		}
-		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "init")
-		if err != nil {
-			log.Panicf("error: terraform init for github failed %s", err)
-		}
-
-		err = pkg.ExecShellWithVars(envs, config.TerraformPath, "apply", "-auto-approve")
-		if err != nil {
-			log.Panicf("error: terraform apply for github failed %s", err)
-		}
-		os.RemoveAll(fmt.Sprintf("%s/.terraform", directory))
-		viper.Set("create.terraformapplied.github", true)
-		viper.WriteConfig()
-	} else {
-		log.Println("Skipping: ApplyGithubTerraform")
+	log.Println("Executing ApplyGithubTerraform")
+	if dryRun {
+		log.Printf("[#99] Dry-run mode, ApplyGithubTerraform skipped.")
+		return
 	}
+	//* AWS_SDK_LOAD_CONFIG=1
+	//* https://registry.terraform.io/providers/hashicorp/aws/2.34.0/docs#shared-credentials-file
+	envs := map[string]string{}
+	envs["AWS_SDK_LOAD_CONFIG"] = "1"
+	envs["AWS_PROFILE"] = viper.GetString("aws.profile")
+	// Prepare for terraform gitlab execution
+	envs["GITHUB_TOKEN"] = os.Getenv("GITHUB_AUTH_TOKEN")
+	envs["GITHUB_OWNER"] = viper.GetString("github.owner")
+	envs["TF_VAR_atlantis_repo_webhook_secret"] = viper.GetString("github.atlantis.webhook.secret")
+	envs["TF_VAR_kubefirst_bot_ssh_public_key"] = viper.GetString("botPublicKey")
+
+	directory := fmt.Sprintf("%s/gitops/terraform/github", config.K1FolderPath)
+	err := os.Chdir(directory)
+	if err != nil {
+		log.Panic("error: could not change directory to " + directory)
+	}
+	err = pkg.ExecShellWithVars(envs, config.TerraformPath, "init")
+	if err != nil {
+		log.Panicf("error: terraform init for github failed %s", err)
+	}
+
+	err = pkg.ExecShellWithVars(envs, config.TerraformPath, "apply", "-auto-approve")
+	if err != nil {
+		log.Panicf("error: terraform apply for github failed %s", err)
+	}
+	os.RemoveAll(fmt.Sprintf("%s/.terraform", directory))
+	viper.Set("github.terraformapplied.gitops", true)
+	viper.WriteConfig()
 }
+
+// todo destroy
