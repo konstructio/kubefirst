c656a9614ef12d0e7adcf3db4a2a2b899448403e
Jéssica Marinho <jlmarinhocosta@gmail.com>
2022-10-17 17:01:26 -0300

    Make github be destroyed through terraform (#571)
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>
    
    Signed-off-by: Jessica Marinho <jessica@kubeshop.io>
    Co-authored-by: Jessica Marinho <jessica@kubeshop.io>

--- cmd/githubRemove.go
+++ cmd/githubRemove.go
@@ -1,15 +1,13 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/kubefirst/kubefirst/internal/flagset"
-	"github.com/kubefirst/kubefirst/internal/githubWrapper"
+	"github.com/kubefirst/kubefirst/internal/github"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -20,34 +18,29 @@ var githubRemoveCmd = &cobra.Command{
 	Short: "Undo github setup",
 	Long:  `TBD`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// todo je terraform destroy
-		fmt.Println("githubRemove called")
-		fmt.Println("Owner used:", viper.GetString("github.owner"))
-		gitWrapper := githubWrapper.New()
-		err := gitWrapper.RemoveRepo(viper.GetString("github.owner"), "gitops")
+		globalFlags, err := flagset.ProcessGlobalFlags(cmd)
 		if err != nil {
 			return err
-		}
-		err = gitWrapper.RemoveRepo(viper.GetString("github.owner"), "metaphor")
-		if err != nil {
-			return err
-		}
-		err = gitWrapper.RemoveRepo(viper.GetString("github.owner"), "metaphor-go")
-		if err != nil {
-			return err
-		}
-		err = gitWrapper.RemoveRepo(viper.GetString("github.owner"), "metaphor-frontend")
-		if err != nil {
-			return err
-		}
-		err = gitWrapper.RemoveSSHKey(viper.GetInt64("github.ssh.keyId"))
-		if err != nil {
-			log.Println("Trying to remove key failed:", err)
 		}
 
-		viper.Set("github.repo.added", false)
-		viper.Set("github.repo.populated", false)
-		viper.WriteConfig()
+		if globalFlags.DryRun {
+			log.Printf("[#99] Dry-run mode, githubRemoveCmd skipped.")
+			return nil
+		}
+
+		log.Println("Org used:", viper.GetString("github.org"))
+		log.Println("dry-run:", globalFlags.DryRun)
+
+		if viper.GetBool("github.terraformapplied.gitops") {
+
+			informUser("Destroying repositories with terraform in GitHub", globalFlags.SilentMode)
+
+			github.DestroyGitHubTerraform(globalFlags.DryRun)
+
+			informUser("GitHub terraform destroyed", globalFlags.SilentMode)
+		}
+
+		log.Printf("GitHub terraform Executed with Success")
 		return nil
 	},
 }
