ed4ce5ed03f7645ba7c2e84909ab86f4d7ba74b0
João Vanzuita <joao@kubeshop.io>
2022-10-18 16:47:08 -0300

    fix: cluster create closing the http connection at the end
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- cmd/create.go
+++ cmd/create.go
@@ -52,10 +52,9 @@ cluster provisioning process spinning up the services, and validates the livenes
 
 		hostedZoneName := viper.GetString("aws.hostedzonename")
 
-		var telemetryHandler handlers.TelemetryHandler
 		if globalFlags.UseTelemetry {
 			// Instantiates a SegmentIO client to send messages to the segment API.
-			segmentIOClient := analytics.New(pkg.SegmentIOWriteKey)
+			segmentIOClientStart := analytics.New(pkg.SegmentIOWriteKey)
 
 			// SegmentIO library works with queue that is based on timing, we explicit close the http client connection
 			// to force flush in case there is still some pending message in the SegmentIO library queue.
@@ -64,9 +63,9 @@ cluster provisioning process spinning up the services, and validates the livenes
 				if err != nil {
 					log.Println(err)
 				}
-			}(segmentIOClient)
+			}(segmentIOClientStart)
 
-			telemetryDomain, err := domain.NewTelemetry(
+			telemetryDomainStart, err := domain.NewTelemetry(
 				pkg.MetricMgmtClusterInstallStarted,
 				hostedZoneName,
 				configs.K1Version,
@@ -74,10 +73,10 @@ cluster provisioning process spinning up the services, and validates the livenes
 			if err != nil {
 				log.Println(err)
 			}
-			telemetryService := services.NewSegmentIoService(segmentIOClient)
-			telemetryHandler := handlers.NewTelemetryHandler(telemetryService)
+			telemetryServiceStart := services.NewSegmentIoService(segmentIOClientStart)
+			telemetryHandlerStart := handlers.NewTelemetryHandler(telemetryServiceStart)
 
-			err = telemetryHandler.SendCountMetric(telemetryDomain)
+			err = telemetryHandlerStart.SendCountMetric(telemetryDomainStart)
 			if err != nil {
 				log.Println(err)
 			}
@@ -171,7 +170,19 @@ cluster provisioning process spinning up the services, and validates the livenes
 		log.Println("sending mgmt cluster install completed metric")
 
 		if globalFlags.UseTelemetry {
-			installCompletedTelemetry, err := domain.NewTelemetry(
+			// Instantiates a SegmentIO client to send messages to the segment API.
+			segmentIOClientCompleted := analytics.New(pkg.SegmentIOWriteKey)
+
+			// SegmentIO library works with queue that is based on timing, we explicit close the http client connection
+			// to force flush in case there is still some pending message in the SegmentIO library queue.
+			defer func(segmentIOClientCompleted analytics.Client) {
+				err := segmentIOClientCompleted.Close()
+				if err != nil {
+					log.Println(err)
+				}
+			}(segmentIOClientCompleted)
+
+			telemetryDomainCompleted, err := domain.NewTelemetry(
 				pkg.MetricMgmtClusterInstallCompleted,
 				hostedZoneName,
 				configs.K1Version,
@@ -179,7 +190,10 @@ cluster provisioning process spinning up the services, and validates the livenes
 			if err != nil {
 				log.Println(err)
 			}
-			err = telemetryHandler.SendCountMetric(installCompletedTelemetry)
+			telemetryServiceCompleted := services.NewSegmentIoService(segmentIOClientCompleted)
+			telemetryHandlerCompleted := handlers.NewTelemetryHandler(telemetryServiceCompleted)
+
+			err = telemetryHandlerCompleted.SendCountMetric(telemetryDomainCompleted)
 			if err != nil {
 				log.Println(err)
 			}
