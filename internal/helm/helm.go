c43188a61eaad19d1066011b9a2dc35bc93b261c
João Vanzuita <joao@kubeshop.io>
2022-10-20 10:53:43 -0300

    fix: argocd install retry logic
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- internal/helm/helm.go
+++ internal/helm/helm.go
@@ -14,49 +14,55 @@ import (
 func InstallArgocd(dryRun bool) error {
 	config := configs.ReadConfig()
 	message := "error installing argo-cd: unexpected state"
-	if !viper.GetBool("argocd.helm.install.complete") {
-		if dryRun {
-			log.Printf("[#99] Dry-run mode, helmInstallArgocd skipped.")
-			return nil
-		}
-		// ! commenting out until a clean execution is necessary // create namespace
-		// Refers to: https://github.com/kubefirst/kubefirst/issues/434
-		totalAttempts := 5
-		for i := 0; i < totalAttempts; i++ {
-			log.Printf("Installing Argo-CD, attempt (%d of %d)", i+1, totalAttempts)
-			_, _, err := pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "repo", "add", "argo", "https://argoproj.github.io/argo-helm")
-			if err != nil {
-				log.Printf("error: could not run helm repo add %s", err)
-				message = "error installing argo-cd: add repo"
-				continue
-			}
 
-			_, _, err = pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "repo", "update")
-			if err != nil {
-				log.Printf("error: could not helm repo update %s", err)
-				message = "error installing argo-cd: update repo"
-				continue
-			}
+	if dryRun {
+		log.Printf("[#99] Dry-run mode, helmInstallArgocd skipped.")
+		return nil
+	}
 
-			_, _, err = pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "upgrade", "--install", "argocd", "--namespace", "argocd", "--create-namespace", "--version", config.ArgoCDChartHelmVersion, "--wait", "--values", fmt.Sprintf("%s/argocd-init-values.yaml", config.K1FolderPath), "argo/argo-cd")
-			if err != nil {
-				log.Printf("error: could not helm install argocd command %s", err)
-				message = "error installing argo-cd: install argo-cd"
-				continue
-			}
-
-			viper.Set("argocd.helm.install.complete", true)
-			err = viper.WriteConfig()
-			if err != nil {
-				log.Printf("error: could not write to viper config")
-				message = "error installing argo-cd: update config"
-				continue
-			}
-		}
-	} else {
+	if viper.GetBool("argocd.helm.install.complete") {
 		log.Printf("[#99] Already created before, helmInstallArgocd skipped.")
 		return nil
 	}
+
+	// ! commenting out until a clean execution is necessary // create namespace
+	// Refers to: https://github.com/kubefirst/kubefirst/issues/434
+	totalAttempts := 5
+	for i := 0; i < totalAttempts; i++ {
+		log.Printf("Installing Argo-CD, attempt (%d of %d)", i+1, totalAttempts)
+		_, _, err := pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "repo", "add", "argo", "https://argoproj.github.io/argo-helm")
+		if err != nil {
+			log.Printf("error: could not run helm repo add %s", err)
+			message = "error installing argo-cd: add repo"
+			continue
+		}
+
+		_, _, err = pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "repo", "update")
+		if err != nil {
+			log.Printf("error: could not helm repo update %s", err)
+			message = "error installing argo-cd: update repo"
+			continue
+		}
+
+		_, _, err = pkg.ExecShellReturnStrings(config.HelmClientPath, "--kubeconfig", config.KubeConfigPath, "upgrade", "--install", "argocd", "--namespace", "argocd", "--create-namespace", "--version", config.ArgoCDChartHelmVersion, "--wait", "--values", fmt.Sprintf("%s/argocd-init-values.yaml", config.K1FolderPath), "argo/argo-cd")
+		if err != nil {
+			log.Printf("error: could not helm install argocd command %s", err)
+			message = "error installing argo-cd: install argo-cd"
+			continue
+		}
+
+		viper.Set("argocd.helm.install.complete", true)
+		err = viper.WriteConfig()
+		if err != nil {
+			log.Printf("error: could not write to viper config")
+			message = "error installing argo-cd: update config"
+			continue
+		}
+		return nil
+	}
+
+	// previous for loop will attempt to install argo, if the attempts fail, it will reach this point, and returns
+	// the default error message
 	return fmt.Errorf(message)
 }
 
