353ea76406c8e4aa45033877574314fff75473fe
João Vanzuita <joao@kubeshop.io>
2022-11-03 10:14:42 -0300

    feat: use viper standard for command inputs
    Signed-off-by: João Vanzuita <joao@kubeshop.io>

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/kubefirst/kubefirst/cmd/local"
+	"github.com/kubefirst/kubefirst/configs"
 	"os"
 
 	"github.com/kubefirst/kubefirst/internal/progressPrinter"
@@ -16,8 +17,11 @@ var rootCmd = &cobra.Command{
 	Long: `kubefirst management cluster installer provisions an
 	open source application delivery platform in under an hour. 
 	checkout the docs at docs.kubefirst.io.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		// wire viper config for flags for all commands
+		return configs.InitializeViperConfig(cmd)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		//log.Println(viper.Get("name"))
 		fmt.Println("To learn more about kubefirst, run:")
 		fmt.Println("  kubefirst help")
 	},
@@ -38,11 +42,5 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize()
-
-	// Cobra also supports local flags, which will only run, when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	// todo: temporary, move it into CLI tree
 	rootCmd.AddCommand(local.NewCommand())
-
 }
