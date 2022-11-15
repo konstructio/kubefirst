4c07b9e3905a88e583114e623d75d70b4f1cd9f3
Jared Edwards <jared@kubefirst.com>
2022-11-15 09:59:25 -0700

    create .kubefirst file during clean (#731)
    * create .kubefirst file during clean
    
    * use config value
    
    * run latest version of kubefirst cli for test

--- cmd/clean.go
+++ cmd/clean.go
@@ -74,6 +74,13 @@ re-create Kubefirst base files. To destroy cloud resources you need to specify a
 			return fmt.Errorf("error: could not create directory %q - it must exist to continue. error is: %s", config.K1FolderPath, err)
 		}
 
+		// re-create .kubefirst file
+		kubefirstFile, err := os.Create(config.KubefirstConfigFilePath)
+		if err != nil {
+			return fmt.Errorf("error: could not create `$HOME/.kubefirst` file:", err)
+		}
+		kubefirstFile.Close()
+
 		// re-create base
 		log.Printf("%q config file and %q folder were deleted and re-created", config.KubefirstConfigFilePath, config.K1FolderPath)
 
