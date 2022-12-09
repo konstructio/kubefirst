16a82a783f9ab010f350d6172b8cafe749d4084b
Jared Edwards <jared@kubefirst.com>
2022-12-08 23:14:16 -0700

    adds output for eks node group role arn

--- pkg/helpers.go
+++ pkg/helpers.go
@@ -128,6 +128,7 @@ func DetokenizeDirectory(path string, fi os.FileInfo, err error) error {
 		bucketGitlabBackup := viper.GetString("bucket.gitlab-backup.name")
 		bucketChartmuseum := viper.GetString("bucket.chartmuseum.name")
 		region := viper.GetString("aws.region")
+		eksNodeGroupArn := viper.GetString("aws.node-group-arn")
 		adminEmail := viper.GetString("adminemail")
 		awsAccountId := viper.GetString("aws.accountid")
 		kmsKeyId := viper.GetString("vault.kmskeyid")
@@ -156,6 +157,7 @@ func DetokenizeDirectory(path string, fi os.FileInfo, err error) error {
 		} else {
 			newContents = strings.Replace(newContents, "<AWS_LIFECYCLE_NODES>", "ON_DEMAND", -1)
 		}
+		newContents = strings.Replace(newContents, "<EKS_NODE_GROUP_ARN>", eksNodeGroupArn, -1)
 
 		githubToken := os.Getenv("KUBEFIRST_GITHUB_AUTH_TOKEN")
 
