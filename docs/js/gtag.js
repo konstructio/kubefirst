4c0f788de48f68e53111bf5a804a023d6ec1e2f5
Frédéric Harper <hi@fred.dev>
2022-12-21 15:57:12 -0500

    switch from Google Tag to Google Analytics
    cannot make Google Tag works, see https://github.com/mkdocs/mkdocs/issues/3057
    
    Signed-off-by: Frédéric Harper <hi@fred.dev>

--- docs/js/gtag.js
+++
@@ -1,9 +0,0 @@
-app.location$.subscribe(function (url) {
-	window.dataLayer = window.dataLayer || [];
-	function gtag() {
-	  dataLayer.push(arguments);
-	}
-	gtag("js", new Date());
-
-	gtag("config", "GTM-KZLF3TJ");
-});
