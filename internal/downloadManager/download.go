e57029feb14dc2510f0c374c3730e8cc3d80ae9e
Thiago Pagotto <pagottoo@gmail.com>
2023-01-04 14:02:57 -0300

    fix typo
    Signed-off-by: Thiago Pagotto <pagottoo@gmail.com>

--- internal/downloadManager/download.go
+++ internal/downloadManager/download.go
@@ -175,7 +175,7 @@ func VerifyCacheTerraform(config *configs.Config) bool {
 	var tfVersion TerraformVersion
 	err := json.Unmarshal(data, &tfVersion)
 	if err != nil {
-		log.Error().Err(err).Msgf("Error unmarshal", err)
+		log.Error().Err(err).Msgf("Error unmarshal: %s", err)
 	}
 
 	if tfVersion.TerraformVersion == config.TerraformVersion {
@@ -207,7 +207,7 @@ func VerifyCacheKubectl(config *configs.Config) bool {
 	var k KubectlVersion
 	err := json.Unmarshal(data, &k)
 	if err != nil {
-		log.Error().Err(err).Msgf("Error unmarshal", err)
+		log.Error().Err(err).Msgf("Error unmarshal: %s", err)
 	}
 
 	if k.ClientVersion.GitVersion == config.KubectlVersion {
