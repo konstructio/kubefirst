26a917955fd79e1136cdcc30453bae4e6cbc6e70
Jared Edwards <jared@kubefirst.com>
2023-02-21 13:38:50 -0700

    adjust command message and fix required flag

--- cmd/k3d/command.go
+++ cmd/k3d/command.go
@@ -52,7 +52,7 @@ func Create() *cobra.Command {
 	createCmd.Flags().StringVar(&clusterNameFlag, "cluster-name", "kubefirst", "the name of the cluster to create")
 	createCmd.Flags().StringVar(&clusterTypeFlag, "cluster-type", "mgmt", "the type of cluster to create (i.e. mgmt|workload)")
 	createCmd.Flags().BoolVar(&dryRun, "dry-run", false, "don't execute the installation")
-	createCmd.Flags().StringVar(&githubOwnerFlag, "github-owner", "", "the GitHub owner of the new gitops and metaphor repositories (required)")
+	createCmd.Flags().StringVar(&githubOwnerFlag, "github-owner", "", "the GitHub owner of the new gitops and metaphor repositories")
 	createCmd.Flags().StringVar(&gitopsTemplateBranchFlag, "gitops-template-branch", "main", "the branch to clone for the gitops-template repository")
 	createCmd.Flags().StringVar(&gitopsTemplateURLFlag, "gitops-template-url", "https://github.com/kubefirst/gitops-template.git", "the fully qualified url to the gitops-template repository to clone")
 	createCmd.Flags().StringVar(&kbotPasswordFlag, "kbot-password", "", "the default password to use for the kbot user")
@@ -67,7 +67,7 @@ func Destroy() *cobra.Command {
 	destroyCmd := &cobra.Command{
 		Use:   "destroy",
 		Short: "destroy the kubefirst platform",
-		Long:  "todo",
+		Long:  "deletes the GitHub resources, k3d resources, and local content to re-provision",
 		RunE:  destroyK3d,
 	}
 
