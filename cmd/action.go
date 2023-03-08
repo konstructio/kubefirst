f82e185800e5c0112b38ef8cbc12ded0e0bf2a05
Jared Edwards <jared@kubefirst.com>
2023-03-07 23:26:43 -0700

    revert actions.go

--- cmd/action.go
+++ cmd/action.go
@@ -1,12 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/cobra"
 )
 
@@ -21,44 +15,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Panic(err)
-		}
-
-		metaphorDir := fmt.Sprintf("%s/.k1/metaphor", homeDir)
-		// os.Mkdir(metaphorDir, 0700)
-		// init
-		// _, err = git.PlainInit(metaphorDir, false)
-		// if err != nil {
-		// 	return err
-		// }
-
-		metaphorRepo, err := git.PlainOpen(metaphorDir)
-
-		w, _ := metaphorRepo.Worktree()
-		branchName := plumbing.NewBranchReferenceName("main")
-		headRef, err := metaphorRepo.Head()
-		if err != nil {
-			return fmt.Errorf("Error Setting reference: %s", err)
-		}
-
-		ref := plumbing.NewHashReference(branchName, headRef.Hash())
-		err = metaphorRepo.Storer.SetReference(ref)
-		if err != nil {
-			return fmt.Errorf("error Storing reference: %s", err)
-		}
-
-		err = w.Checkout(&git.CheckoutOptions{Branch: ref.Name()})
-		if err != nil {
-			return fmt.Errorf("error checking out main: %s", err)
-		}
-
-		// metaphorRepo, err = gitClient.SetRefToMainBranch(metaphorRepo)
-		// if err != nil {
-		// 	return err
-		// }
 
 		return nil
 	},
