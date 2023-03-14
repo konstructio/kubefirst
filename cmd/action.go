ab773eda2ac9c41880940b95568b41cc67618844
Scott Hawkins <scott@echoboomer.net>
2023-03-14 15:07:47 -0400

    get argocd imports working

--- cmd/action.go
+++ cmd/action.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
-	argo "github.com/argoproj/argo-cd/pkg/client/clientset/versioned"
+	"context"
+
+	v1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	argo "github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned"
 	"github.com/kubefirst/kubefirst/internal/aws"
 	"github.com/kubefirst/kubefirst/internal/k8s"
 	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // actionCmd represents the action command
@@ -20,13 +24,60 @@ to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		config := aws.GetConfig("kubefirst-tech")
 
-		config, err := k8s.GetClientConfig(false, config.Kubeconfig)
+		kubeconfig, err := k8s.GetClientConfig(false, config.Kubeconfig)
 		if err != nil {
 			return err
 		}
 
-		argo.NewForConfig()
+		argoClient, err := argo.NewForConfig(kubeconfig)
+		if err != nil {
+			return err
+		}
 
+		app := &v1alpha1.Application{
+			TypeMeta: v1.TypeMeta{
+				Kind:       "Application",
+				APIVersion: "argoproj.io/v1alpha1",
+			},
+			ObjectMeta: v1.ObjectMeta{
+				Name:        "registry",
+				Namespace:   "argocd",
+				Annotations: map[string]string{"argocd.argoproj.io/sync-wave": "1"},
+			},
+			Spec: v1alpha1.ApplicationSpec{
+				Source: &v1alpha1.ApplicationSource{
+					RepoURL:        "",
+					Path:           "",
+					TargetRevision: "",
+					Helm:           &v1alpha1.ApplicationSourceHelm{},
+					Kustomize:      &v1alpha1.ApplicationSourceKustomize{},
+					Directory:      &v1alpha1.ApplicationSourceDirectory{},
+					Plugin:         &v1alpha1.ApplicationSourcePlugin{},
+					Chart:          "",
+					Ref:            "",
+				},
+				Destination: v1alpha1.ApplicationDestination{
+					Server:    "",
+					Namespace: "",
+					Name:      "",
+				},
+				Project: "default",
+				SyncPolicy: &v1alpha1.SyncPolicy{
+					Automated:                &v1alpha1.SyncPolicyAutomated{},
+					SyncOptions:              []string{},
+					Retry:                    &v1alpha1.RetryStrategy{},
+					ManagedNamespaceMetadata: &v1alpha1.ManagedNamespaceMetadata{},
+				},
+				IgnoreDifferences:    []v1alpha1.ResourceIgnoreDifferences{},
+				Info:                 []v1alpha1.Info{},
+				RevisionHistoryLimit: new(int64),
+				Sources:              []v1alpha1.ApplicationSource{},
+			},
+		}
+		_, err = argoClient.ArgoprojV1alpha1().Applications("default").Create(context.Background(), app, v1.CreateOptions{})
+		if err != nil {
+			return err
+		}
 		return nil
 	},
 }
