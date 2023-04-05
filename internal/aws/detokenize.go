0629747df6473c39e5e1ba58f97fab76b3ff78ba
John Dietz <john@kubefirst.com>
2023-04-05 17:11:58 -0400

    adding protection of .git folder to detokenization strategy throughout

--- internal/aws/detokenize.go
+++ internal/aws/detokenize.go
@@ -35,73 +35,81 @@ func detokenizeGitops(path string, tokens *GitOpsDirectoryValues) filepath.WalkF
 
 		// var matched bool
 		matched, _ := filepath.Match("*", fi.Name())
+
 		if matched {
-			read, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+			// ignore .git files
+			isGit := strings.Contains(path, "/.git/")
+			if isGit {
+				matched = false
+				// println("skipping", path)
+			} else {
+				read, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
-			newContents := string(read)
-			newContents = strings.Replace(newContents, "<ALERTS_EMAIL>", tokens.AlertsEmail, -1)
-			newContents = strings.Replace(newContents, "<ATLANTIS_ALLOW_LIST>", tokens.AtlantisAllowList, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
-			newContents = strings.Replace(newContents, "<CLOUD_PROVIDER>", tokens.CloudProvider, -1)
-			newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_ID>", tokens.ClusterId, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_TYPE>", tokens.ClusterType, -1)
-			newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
-			newContents = strings.Replace(newContents, "<KUBE_CONFIG_PATH>", tokens.Kubeconfig, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_ARTIFACTS_BUCKET>", tokens.KubefirstArtifactsBucket, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_STATE_STORE_BUCKET>", tokens.KubefirstStateStoreBucket, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_TEAM>", tokens.KubefirstTeam, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_VERSION>", tokens.KubefirstVersion, -1)
+				newContents := string(read)
+				newContents = strings.Replace(newContents, "<ALERTS_EMAIL>", tokens.AlertsEmail, -1)
+				newContents = strings.Replace(newContents, "<ATLANTIS_ALLOW_LIST>", tokens.AtlantisAllowList, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
+				newContents = strings.Replace(newContents, "<CLOUD_PROVIDER>", tokens.CloudProvider, -1)
+				newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_ID>", tokens.ClusterId, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_TYPE>", tokens.ClusterType, -1)
+				newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
+				newContents = strings.Replace(newContents, "<KUBE_CONFIG_PATH>", tokens.Kubeconfig, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_ARTIFACTS_BUCKET>", tokens.KubefirstArtifactsBucket, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_STATE_STORE_BUCKET>", tokens.KubefirstStateStoreBucket, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_TEAM>", tokens.KubefirstTeam, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_VERSION>", tokens.KubefirstVersion, -1)
 
-			newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_URL>", tokens.ArgoCDIngressURL, -1)
-			newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_NO_HTTP_URL>", tokens.ArgoCDIngressNoHTTPSURL, -1)
-			newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_URL>", tokens.ArgoWorkflowsIngressURL, -1)
-			newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_NO_HTTPS_URL>", tokens.ArgoWorkflowsIngressNoHTTPSURL, -1)
-			newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_URL>", tokens.AtlantisIngressURL, -1)
-			newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_NO_HTTPS_URL>", tokens.AtlantisIngressNoHTTPSURL, -1)
-			newContents = strings.Replace(newContents, "<CHARTMUSEUM_INGRESS_URL>", tokens.ChartMuseumIngressURL, -1)
-			newContents = strings.Replace(newContents, "<VAULT_INGRESS_URL>", tokens.VaultIngressURL, -1)
-			newContents = strings.Replace(newContents, "<VAULT_INGRESS_NO_HTTPS_URL>", tokens.VaultIngressNoHTTPSURL, -1)
-			newContents = strings.Replace(newContents, "<VOUCH_INGRESS_URL>", tokens.VouchIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_URL>", tokens.ArgoCDIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_NO_HTTP_URL>", tokens.ArgoCDIngressNoHTTPSURL, -1)
+				newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_URL>", tokens.ArgoWorkflowsIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_NO_HTTPS_URL>", tokens.ArgoWorkflowsIngressNoHTTPSURL, -1)
+				newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_URL>", tokens.AtlantisIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_NO_HTTPS_URL>", tokens.AtlantisIngressNoHTTPSURL, -1)
+				newContents = strings.Replace(newContents, "<CHARTMUSEUM_INGRESS_URL>", tokens.ChartMuseumIngressURL, -1)
+				newContents = strings.Replace(newContents, "<VAULT_INGRESS_URL>", tokens.VaultIngressURL, -1)
+				newContents = strings.Replace(newContents, "<VAULT_INGRESS_NO_HTTPS_URL>", tokens.VaultIngressNoHTTPSURL, -1)
+				newContents = strings.Replace(newContents, "<VOUCH_INGRESS_URL>", tokens.VouchIngressURL, -1)
 
-			newContents = strings.Replace(newContents, "<AWS_ACCOUNT_ID>", tokens.AwsAccountID, -1)
-			newContents = strings.Replace(newContents, "<AWS_IAM_ARN_ACCOUNT_ROOT>", tokens.AwsIamArnAccountRoot, -1)
-			newContents = strings.Replace(newContents, "<AWS_NODE_CAPACITY_TYPE>", tokens.AwsNodeCapacityType, -1)
+				newContents = strings.Replace(newContents, "<AWS_ACCOUNT_ID>", tokens.AwsAccountID, -1)
+				newContents = strings.Replace(newContents, "<AWS_IAM_ARN_ACCOUNT_ROOT>", tokens.AwsIamArnAccountRoot, -1)
+				newContents = strings.Replace(newContents, "<AWS_NODE_CAPACITY_TYPE>", tokens.AwsNodeCapacityType, -1)
 
-			newContents = strings.Replace(newContents, "<GIT_DESCRIPTION>", tokens.GitDescription, -1)
-			newContents = strings.Replace(newContents, "<GIT_NAMESPACE>", tokens.GitNamespace, -1)
-			newContents = strings.Replace(newContents, "<GIT_PROVIDER>", tokens.GitProvider, -1)
-			newContents = strings.Replace(newContents, "<GIT_RUNNER>", tokens.GitRunner, -1)
-			newContents = strings.Replace(newContents, "<GIT_RUNNER_DESCRIPTION>", tokens.GitRunnerDescription, -1)
-			newContents = strings.Replace(newContents, "<GIT_RUNNER_NS>", tokens.GitRunnerNS, -1)
-			newContents = strings.Replace(newContents, "<GIT_URL>", tokens.GitURL, -1)
+				newContents = strings.Replace(newContents, "<GIT_DESCRIPTION>", tokens.GitDescription, -1)
+				newContents = strings.Replace(newContents, "<GIT_NAMESPACE>", tokens.GitNamespace, -1)
+				newContents = strings.Replace(newContents, "<GIT_PROVIDER>", tokens.GitProvider, -1)
+				newContents = strings.Replace(newContents, "<GIT_RUNNER>", tokens.GitRunner, -1)
+				newContents = strings.Replace(newContents, "<GIT_RUNNER_DESCRIPTION>", tokens.GitRunnerDescription, -1)
+				newContents = strings.Replace(newContents, "<GIT_RUNNER_NS>", tokens.GitRunnerNS, -1)
+				newContents = strings.Replace(newContents, "<GIT_URL>", tokens.GitURL, -1)
 
-			newContents = strings.Replace(newContents, "<GITHUB_HOST>", tokens.GitHubHost, -1)
-			newContents = strings.Replace(newContents, "<GITHUB_OWNER>", strings.ToLower(tokens.GitHubOwner), -1)
-			newContents = strings.Replace(newContents, "<GITHUB_USER>", tokens.GitHubUser, -1)
+				newContents = strings.Replace(newContents, "<GITHUB_HOST>", tokens.GitHubHost, -1)
+				newContents = strings.Replace(newContents, "<GITHUB_OWNER>", strings.ToLower(tokens.GitHubOwner), -1)
+				newContents = strings.Replace(newContents, "<GITHUB_USER>", tokens.GitHubUser, -1)
 
-			newContents = strings.Replace(newContents, "<GITLAB_HOST>", tokens.GitlabHost, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_OWNER>", tokens.GitlabOwner, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_OWNER_GROUP_ID>", strconv.Itoa(tokens.GitlabOwnerGroupID), -1)
-			newContents = strings.Replace(newContents, "<GITLAB_USER>", tokens.GitlabUser, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_HOST>", tokens.GitlabHost, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_OWNER>", tokens.GitlabOwner, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_OWNER_GROUP_ID>", strconv.Itoa(tokens.GitlabOwnerGroupID), -1)
+				newContents = strings.Replace(newContents, "<GITLAB_USER>", tokens.GitlabUser, -1)
 
-			newContents = strings.Replace(newContents, "<GITOPS_REPO_ATLANTIS_WEBHOOK_URL>", tokens.GitOpsRepoAtlantisWebhookURL, -1)
-			newContents = strings.Replace(newContents, "<GITOPS_REPO_GIT_URL>", tokens.GitOpsRepoGitURL, -1)
-			newContents = strings.Replace(newContents, "<GITOPS_REPO_NO_HTTPS_URL>", tokens.GitOpsRepoNoHTTPSURL, -1)
+				newContents = strings.Replace(newContents, "<GITOPS_REPO_ATLANTIS_WEBHOOK_URL>", tokens.GitOpsRepoAtlantisWebhookURL, -1)
+				newContents = strings.Replace(newContents, "<GITOPS_REPO_GIT_URL>", tokens.GitOpsRepoGitURL, -1)
+				newContents = strings.Replace(newContents, "<GITOPS_REPO_NO_HTTPS_URL>", tokens.GitOpsRepoNoHTTPSURL, -1)
 
-			newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", metaphorDevelopmentIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", metaphorProductionIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", metaphorStagingIngressURL, -1)
-			newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1)
-			newContents = strings.Replace(newContents, "<USE_TELEMETRY>", tokens.UseTelemetry, -1)
-
-			err = ioutil.WriteFile(path, []byte(newContents), 0)
-			if err != nil {
-				return err
+				newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", metaphorDevelopmentIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", metaphorProductionIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", metaphorStagingIngressURL, -1)
+				newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1)
+				newContents = strings.Replace(newContents, "<USE_TELEMETRY>", tokens.UseTelemetry, -1)
+				// fmt.Println("matched", path)
+				err = ioutil.WriteFile(path, []byte(newContents), 0)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
@@ -131,25 +139,33 @@ func detokenizeGitopsMetaphor(metaphorDir string, tokens *MetaphorTokenValues) f
 
 		// var matched bool
 		matched, _ := filepath.Match("*", fi.Name())
+
 		if matched {
-			read, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+			// ignore .git files
+			isGit := strings.Contains(path, "/.git/")
+			if isGit {
+				matched = false
+				// println("skipping", path)
+			} else {
+				read, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
-			// todo reduce to terraform tokens by moving to helm chart?
-			newContents := string(read)
-			newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
-			newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1) // todo need to fix metaphor repo names
-			newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
-			newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
+				// todo reduce to terraform tokens by moving to helm chart?
+				newContents := string(read)
+				newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
+				newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1) // todo need to fix metaphor repo names
+				newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
+				newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
 
-			err = ioutil.WriteFile(path, []byte(newContents), 0)
-			if err != nil {
-				return err
+				err = ioutil.WriteFile(path, []byte(newContents), 0)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
--- internal/k3d/detokenize.go
+++ internal/k3d/detokenize.go
@@ -34,46 +34,54 @@ func detokenizeGitops(path string, tokens *GitopsTokenValues) filepath.WalkFunc 
 
 		// var matched bool
 		matched, _ := filepath.Match("*", fi.Name())
+
 		if matched {
-			read, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+			// ignore .git files
+			isGit := strings.Contains(path, "/.git/")
+			if isGit {
+				matched = false
+				// println("skipping", path)
+			} else {
+				read, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
-			// todo reduce to terraform tokens by moving to helm chart?
-			newContents := string(read)
-			newContents = strings.Replace(newContents, "<ALERTS_EMAIL>", "your@email.com", -1) //
-			newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_URL>", tokens.ArgocdIngressURL, -1)
-			newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_URL>", tokens.ArgoWorkflowsIngressURL, -1)
-			newContents = strings.Replace(newContents, "<ATLANTIS_ALLOW_LIST>", tokens.AtlantisAllowList, -1)
-			newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_URL>", tokens.AtlantisIngressURL, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
-			newContents = strings.Replace(newContents, "<CLOUD_PROVIDER>", tokens.CloudProvider, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_ID>", tokens.ClusterId, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_TYPE>", tokens.ClusterType, -1)
-			newContents = strings.Replace(newContents, "<DOMAIN_NAME>", DomainName, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_TEAM>", tokens.KubefirstTeam, -1)
-			newContents = strings.Replace(newContents, "<KUBEFIRST_VERSION>", configs.K1Version, -1)
-			newContents = strings.Replace(newContents, "<KUBE_CONFIG_PATH>", tokens.KubeconfigPath, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
-			newContents = strings.Replace(newContents, "<GITHUB_HOST>", tokens.GithubHost, -1)
-			newContents = strings.Replace(newContents, "<GITHUB_OWNER>", strings.ToLower(tokens.GithubOwner), -1)
-			newContents = strings.Replace(newContents, "<GITHUB_USER>", tokens.GithubUser, -1)
-			newContents = strings.Replace(newContents, "<GIT_PROVIDER>", tokens.GitProvider, -1)
-			newContents = strings.Replace(newContents, "<GITOPS_REPO_GIT_URL>", tokens.GitopsRepoGitURL, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_HOST>", tokens.GitlabHost, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_OWNER>", tokens.GitlabOwner, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_USER>", tokens.GitlabUser, -1)
-			newContents = strings.Replace(newContents, "<GITLAB_OWNER_GROUP_ID>", strconv.Itoa(tokens.GitlabOwnerGroupID), -1)
-			newContents = strings.Replace(newContents, "<VAULT_INGRESS_URL>", tokens.VaultIngressURL, -1)
-			newContents = strings.Replace(newContents, "<USE_TELEMETRY>", tokens.UseTelemetry, -1)
-			newContents = strings.Replace(newContents, "<K3D_DOMAIN>", DomainName, -1)
+				// todo reduce to terraform tokens by moving to helm chart?
+				newContents := string(read)
+				newContents = strings.Replace(newContents, "<ALERTS_EMAIL>", "your@email.com", -1) //
+				newContents = strings.Replace(newContents, "<ARGOCD_INGRESS_URL>", tokens.ArgocdIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ARGO_WORKFLOWS_INGRESS_URL>", tokens.ArgoWorkflowsIngressURL, -1)
+				newContents = strings.Replace(newContents, "<ATLANTIS_ALLOW_LIST>", tokens.AtlantisAllowList, -1)
+				newContents = strings.Replace(newContents, "<ATLANTIS_INGRESS_URL>", tokens.AtlantisIngressURL, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
+				newContents = strings.Replace(newContents, "<CLOUD_PROVIDER>", tokens.CloudProvider, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_ID>", tokens.ClusterId, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_TYPE>", tokens.ClusterType, -1)
+				newContents = strings.Replace(newContents, "<DOMAIN_NAME>", DomainName, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_TEAM>", tokens.KubefirstTeam, -1)
+				newContents = strings.Replace(newContents, "<KUBEFIRST_VERSION>", configs.K1Version, -1)
+				newContents = strings.Replace(newContents, "<KUBE_CONFIG_PATH>", tokens.KubeconfigPath, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
+				newContents = strings.Replace(newContents, "<GITHUB_HOST>", tokens.GithubHost, -1)
+				newContents = strings.Replace(newContents, "<GITHUB_OWNER>", strings.ToLower(tokens.GithubOwner), -1)
+				newContents = strings.Replace(newContents, "<GITHUB_USER>", tokens.GithubUser, -1)
+				newContents = strings.Replace(newContents, "<GIT_PROVIDER>", tokens.GitProvider, -1)
+				newContents = strings.Replace(newContents, "<GITOPS_REPO_GIT_URL>", tokens.GitopsRepoGitURL, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_HOST>", tokens.GitlabHost, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_OWNER>", tokens.GitlabOwner, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_USER>", tokens.GitlabUser, -1)
+				newContents = strings.Replace(newContents, "<GITLAB_OWNER_GROUP_ID>", strconv.Itoa(tokens.GitlabOwnerGroupID), -1)
+				newContents = strings.Replace(newContents, "<VAULT_INGRESS_URL>", tokens.VaultIngressURL, -1)
+				newContents = strings.Replace(newContents, "<USE_TELEMETRY>", tokens.UseTelemetry, -1)
+				newContents = strings.Replace(newContents, "<K3D_DOMAIN>", DomainName, -1)
 
-			err = ioutil.WriteFile(path, []byte(newContents), 0)
-			if err != nil {
-				return err
+				err = ioutil.WriteFile(path, []byte(newContents), 0)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
@@ -103,18 +111,26 @@ func postRunDetokenizeGitops(path string, tokens *GitopsTokenValues) filepath.Wa
 
 		// var matched bool
 		matched, _ := filepath.Match("*", fi.Name())
-		if matched {
-			read, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
 
-			//change Minio post cluster launch to cluster svc address
-			newContents := string(read)
-			newContents = strings.Replace(newContents, fmt.Sprintf("https://minio.%s", DomainName), "http://minio.minio.svc.cluster.local:9000", -1)
-			err = ioutil.WriteFile(path, []byte(newContents), 0)
-			if err != nil {
-				return err
+		if matched {
+			// ignore .git files
+			isGit := strings.Contains(path, "/.git/")
+			if isGit {
+				matched = false
+				// println("skipping", path)
+			} else {
+				read, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+				//change Minio post cluster launch to cluster svc address
+				newContents := string(read)
+				newContents = strings.Replace(newContents, fmt.Sprintf("https://minio.%s", DomainName), "http://minio.minio.svc.cluster.local:9000", -1)
+				err = ioutil.WriteFile(path, []byte(newContents), 0)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
@@ -144,25 +160,33 @@ func detokenize(metaphorDir string, tokens *MetaphorTokenValues) filepath.WalkFu
 
 		// var matched bool
 		matched, _ := filepath.Match("*", fi.Name())
+
 		if matched {
-			read, err := ioutil.ReadFile(path)
-			if err != nil {
-				return err
-			}
+			// ignore .git files
+			isGit := strings.Contains(path, "/.git/")
+			if isGit {
+				matched = false
+				// println("skipping", path)
+			} else {
+				read, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
 
-			// todo reduce to terraform tokens by moving to helm chart?
-			newContents := string(read)
-			newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
-			newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
-			newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1) // todo need to fix metaphor repo names
-			newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
-			newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
-			newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
+				// todo reduce to terraform tokens by moving to helm chart?
+				newContents := string(read)
+				newContents = strings.Replace(newContents, "<METAPHOR_DEVELOPMENT_INGRESS_URL>", tokens.MetaphorDevelopmentIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_STAGING_INGRESS_URL>", tokens.MetaphorStagingIngressURL, -1)
+				newContents = strings.Replace(newContents, "<METAPHOR_PRODUCTION_INGRESS_URL>", tokens.MetaphorProductionIngressURL, -1)
+				newContents = strings.Replace(newContents, "<CONTAINER_REGISTRY_URL>", tokens.ContainerRegistryURL, -1) // todo need to fix metaphor repo names
+				newContents = strings.Replace(newContents, "<DOMAIN_NAME>", tokens.DomainName, -1)
+				newContents = strings.Replace(newContents, "<CLOUD_REGION>", tokens.CloudRegion, -1)
+				newContents = strings.Replace(newContents, "<CLUSTER_NAME>", tokens.ClusterName, -1)
 
-			err = ioutil.WriteFile(path, []byte(newContents), 0)
-			if err != nil {
-				return err
+				err = ioutil.WriteFile(path, []byte(newContents), 0)
+				if err != nil {
+					return err
+				}
 			}
 		}
 		return nil
