3fd1819adbc7a57c1147df3737b75974ecd25017
Scott Hawkins <scott@echoboomer.net>
2023-06-22 09:50:30 -0400

    fix: fix bug where launch tries to create an empty kubeconfig (#1639)

---
+++ internal/launch/constants.go
@@ -0,0 +1,17 @@
+/*
+Copyright (C) 2021-2023, Kubefirst
+
+This program is licensed under MIT.
+See the LICENSE file for more details.
+*/
+package launch
+
+const (
+	consoleURL        = "https://console.kubefirst.dev"
+	helmChartName     = "kubefirst"
+	helmChartRepoName = "kubefirst"
+	helmChartRepoURL  = "https://charts.kubefirst.com"
+	helmChartVersion  = "2.1.7"
+	namespace         = "kubefirst"
+	secretName        = "kubefirst-initial-secrets"
+)
