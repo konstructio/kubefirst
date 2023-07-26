bcaed2914f4744a19ed7953e1c2ff16bde19687e
Jared Edwards <jared@kubefirst.com>
2023-07-25 21:02:12 -0600

    intermitent commit

--- cmd/generate/generate.go
+++ cmd/generate/generate.go
@@ -2,11 +2,25 @@ package generate
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/kubefirst/runtime/pkg/gitClient"
 	"github.com/spf13/cobra"
 )
 
 func generate(cmd *cobra.Command, args []string) error {
-	fmt.Println("hello generate. lets do this")
+
+	fmt.Println("clone git repo")
+	const localGitopsPath = "/Users/jared/basura/gitops-templates/"
+
+	gitopsRepo, err := gitClient.CloneWithTokenAuth(os.Getenv("GITHUB_TOKEN"), "main", localGitopsPath, "https://github.com/your-company-io/gitops.git")
+	if err != nil {
+		fmt.Errorf("error: ", err)
+	}
+	fmt.Println(gitopsRepo)
+
+	fmt.Println("read secrets from vault")
+	fmt.Println("detokenize")
+
 	return nil
 }
