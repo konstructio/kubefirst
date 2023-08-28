86f2c53cd68d914176bf8507b569e9681903d49a
CristhianF7 <CristhianF7@gmail.com>
2023-08-28 18:51:20 -0500

    chore: update helm chart version 2.2.16

--- internal/launch/constants.go
+++ internal/launch/constants.go
@@ -11,7 +11,7 @@ const (
 	helmChartName     = "kubefirst"
 	helmChartRepoName = "kubefirst"
 	helmChartRepoURL  = "https://charts.kubefirst.com"
-	helmChartVersion  = "2.2.15"
+	helmChartVersion  = "2.2.16"
 	namespace         = "kubefirst"
 	secretName        = "kubefirst-initial-secrets"
 )
