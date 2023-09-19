2985dde4a93bc6b47b2ded82b3a10ebbc224c428
Frédéric Harper <hi@fred.dev>
2023-09-19 16:48:10 -0400

    remove presets, we just care about title length

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,6 @@
 const rules = require('@commitlint/rules');
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'header-max-length': [2, 'always', 72],
   },
