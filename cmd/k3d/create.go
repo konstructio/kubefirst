93e85630236d58ed677a342cf5b62694f0c19dd3
John Dietz <john@kubefirst.com>
2023-11-27 15:42:55 -0500

    remove unused event

--- cmd/k3d/create.go
+++ cmd/k3d/create.go
@@ -329,7 +329,6 @@ func runK3d(cmd *cobra.Command, args []string) error {
 
 	segClient := segment.InitClient(clusterId, clusterTypeFlag, gitProviderFlag)
 	defer segClient.Client.Close()
-	//telemetry.SendEvent(segClient, telemetry.ClusterInstallStarted, "")
 
 	// Progress output
 	progressPrinter.AddTracker("preflight-checks", "Running preflight checks", 5)
