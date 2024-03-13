e1238a550bb18390e408bac44fe86b53cdce6063
Cristhian Fernández <CristhianF7@gmail.com>
2024-03-12 23:19:48 -0500

    chore: bump launch (#2111)

--- internal/launch/constants.go
+++ internal/launch/constants.go
@@ -11,7 +11,7 @@ const (
 	helmChartName     = "kubefirst"
 	helmChartRepoName = "kubefirst"
 	helmChartRepoURL  = "https://charts.kubefirst.com"
-	helmChartVersion  = "2.3.9-rc23"
+	helmChartVersion  = "2.3.9-rc25"
 	namespace         = "kubefirst"
 	secretName        = "kubefirst-initial-secrets"
 )
