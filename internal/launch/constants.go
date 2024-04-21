1b41935066d8d8854bdb39cce28a68b571500961
John Dietz <john@kubefirst.com>
2024-04-21 09:59:27 -0400

    adopt 2.4.6 chart

--- internal/launch/constants.go
+++ internal/launch/constants.go
@@ -11,7 +11,7 @@ const (
 	helmChartName     = "kubefirst"
 	helmChartRepoName = "kubefirst"
 	helmChartRepoURL  = "https://charts.kubefirst.com"
-	helmChartVersion  = "2.4.5"
+	helmChartVersion  = "2.4.6"
 	namespace         = "kubefirst"
 	secretName        = "kubefirst-initial-secrets"
 )
