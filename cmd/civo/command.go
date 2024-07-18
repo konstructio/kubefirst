6439bbdc51c19ddcc53cf600239add9f39e01bde
jokestax <mrrishi373@gmail.com>
2024-07-18 06:50:23 +0000

    rebased

--- cmd/civo/command.go
+++ cmd/civo/command.go
@@ -35,7 +35,14 @@ var (
 	nodeTypeFlag             string
 	nodeCountFlag            string
 	installCatalogApps       string
+<<<<<<< HEAD
 	installKubefirstProFlag  bool
+=======
+	gitopsRepoName			 string
+	metaphorRepoName		 string
+	adminTeamName			 string
+	developerTeamName 		 string
+>>>>>>> 61cb972 (added configs)
 
 	// RootCredentials
 	copyArgoCDPasswordToClipboardFlag bool
@@ -105,6 +112,10 @@ func Create() *cobra.Command {
 	createCmd.Flags().StringVar(&subdomainNameFlag, "subdomain", "", "the subdomain to use for DNS records (Cloudflare)")
 	createCmd.Flags().StringVar(&domainNameFlag, "domain-name", "", "the Civo DNS Name to use for DNS records (i.e. your-domain.com|subdomain.your-domain.com) (required)")
 	createCmd.MarkFlagRequired("domain-name")
+	createCmd.Flags().StringVar(&gitopsRepoName,"gitopsRepoName","gitops","the custom gitops name")
+	createCmd.Flags().StringVar(&metaphorRepoName,"metaphorRepoName","metaphor","the custom metaphor name")
+	createCmd.Flags().StringVar(&adminTeamName,"adminTeamName","admins","admin team name for this repo")
+	createCmd.Flags().StringVar(&developerTeamName,"developerTeamName","developers","developer team name for this repo")
 	createCmd.Flags().StringVar(&gitProviderFlag, "git-provider", "github", fmt.Sprintf("the git provider - one of: %s", supportedGitProviders))
 	createCmd.Flags().StringVar(&gitProtocolFlag, "git-protocol", "ssh", fmt.Sprintf("the git protocol - one of: %s", supportedGitProtocolOverride))
 	createCmd.Flags().StringVar(&githubOrgFlag, "github-org", "", "the GitHub organization for the new gitops and metaphor repositories - required if using github")
