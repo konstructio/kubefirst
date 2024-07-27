548684d5c127322b6ad64bb3831d6a865d9569b7
kube1st <kube1st@kubefirst.io>
2024-07-27 02:58:30 +0000

    set next macro chart for kubefirst - 2.4.14-rc23

--- internal/launch/constants.go
+++ internal/launch/constants.go
@@ -11,7 +11,7 @@ const (
 	helmChartName     = "kubefirst"
 	helmChartRepoName = "kubefirst"
 	helmChartRepoURL  = "https://charts.kubefirst.com"
-	helmChartVersion  = "2.4.14-rc22"
+	helmChartVersion  = "2.4.14-rc23"
 	namespace         = "kubefirst"
 	secretName        = "kubefirst-initial-secrets"
 )
