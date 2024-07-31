d2e70ee8eef82df3fb3f6118b179c279b17a57ca
CristhianF7 <CristhianF7@gmail.com>
2024-07-30 21:47:03 -0500

    chore: bubbletea blacklist ci flag

--- main.go
+++ main.go
@@ -120,6 +120,7 @@ func main() {
 	viper.Set("k1-paths.logs-dir", logsFolder)
 	viper.Set("k1-paths.log-file", logfile)
 	viper.Set("k1-paths.log-file-name", logfileName)
+	viper.Set("k1-canRunBubbleTea", canRunBubbleTea)
 
 	if err := viper.WriteConfig(); err != nil {
 		log.Error().Msgf("failed to write config: %v", err)
