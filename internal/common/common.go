e7ab4cc867e600de337698813872a87fd58c3e8e
CristhianF7 <CristhianF7@gmail.com>
2024-08-01 20:38:04 -0500

    chore: test without provisioning

--- internal/common/common.go
+++ internal/common/common.go
@@ -102,9 +102,7 @@ func versionCheck() (res *CheckResponse, skip bool) {
 func GetRootCredentials(cmd *cobra.Command, args []string) error {
 	clusterName := viper.GetString("flags.cluster-name")
 
-	fmt.Print(clusterName)
-
-	cluster, err := cluster.GetCluster("jd-test-4-axmhv")
+	cluster, err := cluster.GetCluster(clusterName)
 	if err != nil {
 		progress.Error(err.Error())
 		return err
