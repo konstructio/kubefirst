9089cbcdeb419f88318455b21de6d94ac3ada88c
nathan-nicholson <nathan@konstruct.io>
2024-12-30 09:07:16 -0600

    chore: remove empty space in conditionals
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>

--- internal/utilities/flags.go
+++ internal/utilities/flags.go
@@ -20,112 +20,94 @@ func GetFlags(cmd *cobra.Command, cloudProvider string) (types.CliFlags, error) 
 
 	alertsEmailFlag, err := cmd.Flags().GetString("alerts-email")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get alerts-email flag: %w", err)
 	}
 
 	cloudRegionFlag, err := cmd.Flags().GetString("cloud-region")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get cloud-region flag: %w", err)
 	}
 
 	clusterNameFlag, err := cmd.Flags().GetString("cluster-name")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get cluster-name flag: %w", err)
 	}
 
 	dnsProviderFlag, err := cmd.Flags().GetString("dns-provider")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get dns-provider flag: %w", err)
 	}
 
 	subdomainFlag, err := cmd.Flags().GetString("subdomain")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get subdomain flag: %w", err)
 	}
 
 	domainNameFlag, err := cmd.Flags().GetString("domain-name")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get domain-name flag: %w", err)
 	}
 
 	githubOrgFlag, err := cmd.Flags().GetString("github-org")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get github-org flag: %w", err)
 	}
 	githubOrgFlag = strings.ToLower(githubOrgFlag)
 
 	gitlabGroupFlag, err := cmd.Flags().GetString("gitlab-group")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get gitlab-group flag: %w", err)
 	}
 	gitlabGroupFlag = strings.ToLower(gitlabGroupFlag)
 
 	gitProviderFlag, err := cmd.Flags().GetString("git-provider")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get git-provider flag: %w", err)
 	}
 
 	gitProtocolFlag, err := cmd.Flags().GetString("git-protocol")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get git-protocol flag: %w", err)
 	}
 
 	gitopsTemplateURLFlag, err := cmd.Flags().GetString("gitops-template-url")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get gitops-template-url flag: %w", err)
 	}
 
 	gitopsTemplateBranchFlag, err := cmd.Flags().GetString("gitops-template-branch")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get gitops-template-branch flag: %w", err)
 	}
 
 	useTelemetryFlag, err := cmd.Flags().GetBool("use-telemetry")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get use-telemetry flag: %w", err)
 	}
 
 	nodeTypeFlag, err := cmd.Flags().GetString("node-type")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get node-type flag: %w", err)
 	}
 
 	installCatalogAppsFlag, err := cmd.Flags().GetString("install-catalog-apps")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get install-catalog-apps flag: %w", err)
 	}
 
 	nodeCountFlag, err := cmd.Flags().GetString("node-count")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get node-count flag: %w", err)
 	}
 
 	installKubefirstProFlag, err := cmd.Flags().GetBool("install-kubefirst-pro")
 	if err != nil {
-
 		return cliFlags, fmt.Errorf("failed to get install-kubefirst-pro flag: %w", err)
 	}
 
 	if cloudProvider == "aws" {
 		ecrFlag, err := cmd.Flags().GetBool("ecr")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get ecr flag: %w", err)
 		}
 
@@ -135,7 +117,6 @@ func GetFlags(cmd *cobra.Command, cloudProvider string) (types.CliFlags, error) 
 	if cloudProvider == "azure" {
 		dnsAzureResourceGroup, err := cmd.Flags().GetString("dns-azure-resource-group")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get dns-azure-resource-group flag: %w", err)
 		}
 		cliFlags.DNSAzureRG = dnsAzureResourceGroup
@@ -144,7 +125,6 @@ func GetFlags(cmd *cobra.Command, cloudProvider string) (types.CliFlags, error) 
 	if cloudProvider == "google" {
 		googleProject, err := cmd.Flags().GetString("google-project")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get google-project flag: %w", err)
 		}
 
@@ -154,35 +134,30 @@ func GetFlags(cmd *cobra.Command, cloudProvider string) (types.CliFlags, error) 
 	if cloudProvider == "k3s" {
 		k3sServersPrivateIps, err := cmd.Flags().GetStringSlice("servers-private-ips")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get servers-private-ips flag: %w", err)
 		}
 		cliFlags.K3sServersPrivateIPs = k3sServersPrivateIps
 
 		k3sServersPublicIps, err := cmd.Flags().GetStringSlice("servers-public-ips")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get servers-public-ips flag: %w", err)
 		}
 		cliFlags.K3sServersPublicIPs = k3sServersPublicIps
 
 		k3sSSHUserFlag, err := cmd.Flags().GetString("ssh-user")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get ssh-user flag: %w", err)
 		}
 		cliFlags.K3sSSHUser = k3sSSHUserFlag
 
 		k3sSSHPrivateKeyFlag, err := cmd.Flags().GetString("ssh-privatekey")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get ssh-privatekey flag: %w", err)
 		}
 		cliFlags.K3sSSHPrivateKey = k3sSSHPrivateKeyFlag
 
 		K3sServersArgsFlags, err := cmd.Flags().GetStringSlice("servers-args")
 		if err != nil {
-
 			return cliFlags, fmt.Errorf("failed to get servers-args flag: %w", err)
 		}
 		cliFlags.K3sServersArgs = K3sServersArgsFlags
