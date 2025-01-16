6d83d839eda4e3d87efbafde17fefebd4d433c53
Patrick D'appollonio <930925+patrickdappollonio@users.noreply.github.com>
2025-01-16 14:25:03 -0500

    Update with checks.

--- cmd/aws/create.go
+++ cmd/aws/create.go
@@ -304,8 +304,7 @@ func createKubernetesAdminRole(ctx context.Context, clusterName string, iamClien
 	// Check if the IAM policy exists
 	cp, err := iamClient.GetPolicy(ctx, &iam.GetPolicyInput{PolicyArn: aws.String(fmt.Sprintf("arn:aws:iam::%s:policy/%s", *callerIdentity.Account, policyName))})
 	if err != nil {
-		var newError *awshttp.ResponseError
-		if errors.As(err, newError) && newError.HTTPStatusCode() == http.StatusNotFound {
+		if newError := (&awshttp.ResponseError{}); errors.As(err, newError) && newError.HTTPStatusCode() == http.StatusNotFound {
 			// Policy does not exist, continue
 		} else {
 			return "", fmt.Errorf("failed to get policy %q: %w", policyName, err)
@@ -353,8 +352,7 @@ func createKubernetesAdminRole(ctx context.Context, clusterName string, iamClien
 	// Check if a role with this name already exists
 	role, err := iamClient.GetRole(ctx, &iam.GetRoleInput{RoleName: aws.String(roleName)})
 	if err != nil {
-		var newError *awshttp.ResponseError
-		if errors.As(err, newError) && newError.HTTPStatusCode() == http.StatusNotFound {
+		if newError := (&awshttp.ResponseError{}); errors.As(err, newError) && newError.HTTPStatusCode() == http.StatusNotFound {
 			// Role does not exist, continue
 		} else {
 			return "", fmt.Errorf("failed to get role %q: %w", roleName, err)
