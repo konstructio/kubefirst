6fc77c5a7215aea953dee39958fa6be2bb1f2b7d
Nathan Nicholson <nathan@konstruct.io>
2025-01-29 11:49:36 -0600

    feat: add stepper for printing messages - KRA-72 (#2400)
    * chore: add replacement for DisplayLogHints and basic test
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: change newlines
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: remove civo bubbletea blacklist
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: fix linting issues
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: remove the codeql workflow in favor of gocilint (#2365)
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * fix: linting issues
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * fix: some minor updates
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: extra whitespace
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    * chore: add missing methods to interface
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>
    
    ---------
    
    Signed-off-by: nathan-nicholson <nathan@konstruct.io>

---
+++ internal/step/stepper.go
@@ -0,0 +1,69 @@
+package step
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/konstructio/cli-utils/stepper"
+)
+
+const (
+	emojiCheck   = "✅"
+	emojiError   = "🔴"
+	emojiMagic   = "✨"
+	emojiHead    = "🤕"
+	emojiNoEntry = "⛔"
+	emojiTada    = "🎉"
+	emojiAlarm   = "⏰"
+	emojiBug     = "🐛"
+	emojiBulb    = "💡"
+	emojiWarning = "⚠️"
+	emojiWrench  = "🔧"
+	emojiBook    = "📘"
+)
+
+type Stepper interface {
+	NewProgressStep(stepName string) *stepper.Step
+	InfoStep(emoji, message string)
+	InfoStepString(message string)
+	DisplayLogHints(cloudProvider string, estimatedTime int)
+}
+
+type Factory struct {
+	writer io.Writer
+}
+
+func NewStepFactory(writer io.Writer) *Factory {
+	return &Factory{writer: writer}
+}
+
+func (s *Factory) NewProgressStep(stepName string) *stepper.Step {
+	return stepper.New(s.writer, stepName)
+}
+
+func (s *Factory) InfoStep(emoji, message string) {
+	fmt.Fprintf(s.writer, "%s %s\n", emoji, message)
+}
+
+func (s *Factory) InfoStepString(message string) {
+	fmt.Fprintf(s.writer, "%s\n", message)
+}
+
+func (s *Factory) DisplayLogHints(cloudProvider string, estimatedTime int) {
+	documentationLink := "https://kubefirst.konstruct.io/docs/"
+
+	if cloudProvider != "" {
+		documentationLink += cloudProvider + `/quick-start/install/cli`
+	}
+
+	header := `
+##
+# Welcome to Kubefirst
+`
+
+	verboseLogs := fmt.Sprintf("### %s To view verbose logs run below command in new terminal: \"kubefirst logs\"\n%s Documentation: %s\n\n", emojiBulb, emojiBook, documentationLink)
+
+	estimatedTimeMsg := fmt.Sprintf("%s Estimated time: %d minutes\n\n", emojiAlarm, estimatedTime)
+
+	s.InfoStepString(fmt.Sprintf("%s%s%s", header, verboseLogs, estimatedTimeMsg))
+}
